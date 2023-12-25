5ed6f57a19358fc508a6927f6cecb4fbfaf46bf3
dreamjz <25699818+dreamjz@users.noreply.github.com>
2023-12-26 01:43:06 +0800

    miniserver: single thread

---
+++ mini-server/src/main.rs
@@ -0,0 +1,45 @@
+use std::{
+    fs,
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
+};
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    println!("Server listening at port: 7878");
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        println!(
+            "Connection established from: {}",
+            stream.peer_addr().unwrap().to_string()
+        );
+
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+
+    let (status_line, filename) = match request_line.as_str() {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "index.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "index.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+    let length = contents.len();
+
+    let resp = format!("{status_line}\r\nContent-length: {length}\r\n\r\n{contents}");
+
+    stream.write_all(resp.as_bytes()).unwrap();
+}
